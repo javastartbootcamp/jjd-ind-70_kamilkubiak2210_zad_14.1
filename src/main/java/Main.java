2f5d79841e858b339a3f51f648246e72aa89c2a0
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-05-27 13:00:45 +0200

    Rozwiązanie.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
+import java.io.IOException;
+import java.util.TreeMap;
+
 public class Main {
 
-
     public static void main(String[] args) {
+        String fileName = "src/main/resources/liczby.txt";
+        try {
+            NumberReader numberReader = new NumberReader();
+            if (!numberReader.listOfNumbers(fileName).isEmpty()) {
+                TreeMap<Integer, Integer> integerMap = numberReader.listOfNumbers(fileName);
+                numberReader.printListOfNumbers(integerMap);
+            } else {
+                System.err.println("Lista jest pusta");
+            }
+        } catch (IOException e) {
+            System.err.println("Nie można znaleźć pliku " + fileName);
+        }
 
     }
 
